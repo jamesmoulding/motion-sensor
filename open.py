5fc8f969bd1b80b08f18b577deb7c86835d4c07c
James Moulding <james.moulding@opensensors.io>
2015-07-31 15:04:04 +0100

    Update open.py

--- open.py
+++ open.py
@@ -3,13 +3,13 @@ import paho.mqtt.client as mqtt
 import time
 
 mqttc = mqtt.Client(client_id="1596")
-mqttc.username_pw_set("jamesmoulding", password="GIz2LXKM")
+mqttc.username_pw_set("YOUR_OPENSENSORS_USERNAME", password="YOUR_DEVICE_PASSWORD")
 mqttc.connect("opensensors.io")
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)  # open first serial port
 while True:
 	message = ser.readline()
 	print message
-	mqttc.publish("/users/jamesmoulding/testing", payload=message, qos=0, retain=False)
+	mqttc.publish("/users/YOUR_OPENSENSORS_USERNAME/YOUR_TOPIC_NAME", payload=message, qos=0, retain=False)
 mqttc.disconnect();
 time.sleep(1);
