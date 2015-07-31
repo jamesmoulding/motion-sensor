0fac52f4c2e2e93a8abe558983e0b490b0f24f03
jamesmoulding <james.moulding@opensensors.io>
2015-07-31 13:15:57 +0100

    sketch file

---
+++ PIR1.ino
@@ -0,0 +1,42 @@
+// Uses a PIR sensor to detect movement, buzzes a buzzer
+// more info here: http://blog.makezine.com/projects/pir-sensor-arduino-alarm/
+// email me, John Park, at jp@jpixl.net
+// based upon:
+// PIR sensor tester by Limor Fried of Adafruit
+// tone code by michael@thegrebs.com
+
+
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+
+void setup() {
+  pinMode(inputPin, INPUT);     // declare sensor as input
+  Serial.begin(9600);
+}
+
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    delay(150);
+
+
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } 
+  else {
+    delay(300);    
+    if (pirState == HIGH){
+      // we have just turned off
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
+}
+
+
